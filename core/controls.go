762ebb69b84bb298004b4fc687d5d733111817df
Luka Košćak <koscak.luka@gmail.com>
2025-11-06 18:31:58 +0200

    feat: alias cancel method to cancel turn in orchestrator

--- core/controls.go
+++ core/controls.go
@@ -1,10 +1,19 @@
 package orchestration
 
-func (o *Orchestrator) Cancel() {
+func (o *Orchestrator) CancelTurn() {
+	// TODO: This could potentially be done directly on the turn instead of
+	// as an exposed method
 	if o.activePrompt != nil {
 		o.canceled = true
 		if o.orchestrateOptions.onCancellation != nil {
 			o.orchestrateOptions.onCancellation()
 		}
 	}
+}
+
+// Cancel is an alias for CancelTurn
+//
+// Deprecated: use CancelTurn instead
+func (o *Orchestrator) Cancel() {
+	o.CancelTurn()
 }
