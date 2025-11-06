be9521eb23e2490ed7cfc8fc43250ac9725c10bc
Luka Košćak <koscak.luka@gmail.com>
2025-11-06 17:32:48 +0200

    part: define turns interface in context package

---
+++ core/context/turns.go
@@ -0,0 +1,14 @@
+package context
+
+import "github.com/koscakluka/ema/core/llms"
+
+type Turns interface {
+	Peek() *llms.Turn
+	Push(turn llms.Turn)
+	Pop() llms.Turn
+
+	Clear()
+
+	Values(yield func(llms.Turn) bool)
+	RValues(yield func(llms.Turn) bool)
+}
