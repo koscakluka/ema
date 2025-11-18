e7b5335508171c60d725b36daa38753af634b384
Luka Košćak <koscak.luka@gmail.com>
2025-11-18 18:13:30 +0200

    refactor: move orchestration user turn processing code to separate file

--- core/orchestration.go
+++ core/orchestration.go
@@ -5,14 +5,11 @@ import (
 	"fmt"
 	"strings"
 	"sync"
-	"time"
 
 	"log"
 
 	emaContext "github.com/koscakluka/ema/core/context"
 	"github.com/koscakluka/ema/core/llms"
-	"github.com/koscakluka/ema/core/speechtotext"
-	"github.com/koscakluka/ema/internal/utils"
 )
 
 type Orchestrator struct {
@@ -92,43 +89,7 @@ func (o *Orchestrator) Orchestrate(ctx context.Context, opts ...OrchestrateOptio
 	}
 
 	o.initTTS()
-
-	if o.speechToTextClient != nil {
-		sttOptions := []speechtotext.TranscriptionOption{
-			speechtotext.WithSpeechStartedCallback(func() {
-				if o.orchestrateOptions.onSpeakingStateChanged != nil {
-					o.orchestrateOptions.onSpeakingStateChanged(true)
-				}
-			}),
-			speechtotext.WithSpeechEndedCallback(func() {
-				if o.orchestrateOptions.onSpeakingStateChanged != nil {
-					o.orchestrateOptions.onSpeakingStateChanged(false)
-				}
-			}),
-			speechtotext.WithInterimTranscriptionCallback(func(transcript string) {
-				// TODO: Start generating interruption here already
-				// marking the ID will probably be required to keep track of it
-
-				if o.orchestrateOptions.onInterimTranscription != nil {
-					o.orchestrateOptions.onInterimTranscription(transcript)
-				}
-			}),
-			speechtotext.WithTranscriptionCallback(func(transcript string) {
-				if o.orchestrateOptions.onInterimTranscription != nil {
-					o.orchestrateOptions.onInterimTranscription("")
-				}
-
-				o.SendPrompt(transcript)
-			}),
-		}
-		if o.audioInput != nil {
-			sttOptions = append(sttOptions, speechtotext.WithEncodingInfo(o.audioInput.EncodingInfo()))
-		}
-
-		if err := o.speechToTextClient.Transcribe(context.TODO(), sttOptions...); err != nil {
-			log.Fatalf("Failed to start transcribing: %v", err)
-		}
-	}
+	o.initSST()
 
 	go func() {
 		for transcript := range o.transcripts {
@@ -188,57 +149,7 @@ func (o *Orchestrator) Orchestrate(ctx context.Context, opts ...OrchestrateOptio
 }
 
 func (o *Orchestrator) SendPrompt(prompt string) {
-	var interruptionID *int64
-	if o.turns.activeTurn() != nil {
-		interruptionID = utils.Ptr(time.Now().UnixNano())
-		interruption := &llms.InterruptionV0{
-			ID:     *interruptionID,
-			Source: prompt,
-		}
-		o.turns.addInterruption(*interruption)
-	}
-
-	passthrough := &prompt
-	if interruptionID != nil {
-		if o.interruptionHandlerV1 != nil {
-			if interruption, err := o.interruptionHandlerV1.HandleV1(*interruptionID, o, o.tools); err != nil {
-				log.Printf("Failed to handle interruption: %v", err)
-			} else {
-				o.turns.updateInterruption(*interruptionID, func(update *llms.InterruptionV0) {
-					update.Type = interruption.Type
-					update.Resolved = interruption.Resolved
-				})
-				return
-			}
-		} else if o.interruptionHandlerV0 != nil {
-			if err := o.interruptionHandlerV0.HandleV0(prompt, o.turns.turns, o.tools, o); err != nil {
-				log.Printf("Failed to handle interruption: %v", err)
-			} else {
-				o.turns.updateInterruption(*interruptionID, func(interruption *llms.InterruptionV0) {
-					interruption.Resolved = true
-				})
-				return
-			}
-		} else if o.interruptionClassifier != nil {
-			interruption, err := o.interruptionClassifier.Classify(prompt, llms.ToMessages(o.turns.turns), ClassifyWithTools(o.tools))
-			if err != nil {
-				// TODO: Retry?
-				log.Printf("Failed to classify interruption: %v", err)
-			} else {
-				o.turns.updateInterruption(*interruptionID, func(i *llms.InterruptionV0) { i.Type = string(interruption) })
-				passthrough, err = o.respondToInterruption(prompt, interruption)
-				if err != nil {
-					log.Printf("Failed to respond to interruption: %v", err)
-				}
-			}
-		}
-		o.turns.updateInterruption(*interruptionID, func(interruption *llms.InterruptionV0) {
-			interruption.Resolved = true
-		})
-	}
-	if passthrough != nil {
-		o.queuePrompt(*passthrough)
-	}
+	o.processUserTurn(prompt)
 }
 
 func (o *Orchestrator) SendAudio(audio []byte) error {
@@ -250,13 +161,6 @@ func (o *Orchestrator) SendAudio(audio []byte) error {
 // for handling prompts that are sure to follow up after the current turn.
 func (o *Orchestrator) QueuePrompt(prompt string) {
 	go o.queuePrompt(prompt)
-}
-
-func (o *Orchestrator) queuePrompt(prompt string) {
-	if o.orchestrateOptions.onTranscription != nil {
-		o.orchestrateOptions.onTranscription(prompt)
-	}
-	o.transcripts <- prompt
 }
 
 func (o *Orchestrator) SetSpeaking(isSpeaking bool) {
---
+++ core/turnprocessinguser.go
@@ -0,0 +1,114 @@
+package orchestration
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/koscakluka/ema/core/llms"
+	"github.com/koscakluka/ema/core/speechtotext"
+	"github.com/koscakluka/ema/internal/utils"
+)
+
+func (o *Orchestrator) initSST() {
+	if o.speechToTextClient != nil {
+		sttOptions := []speechtotext.TranscriptionOption{
+			speechtotext.WithSpeechStartedCallback(func() {
+				// TODO: Consider pausing on speech start
+				// maybe with some wait time for interim transcript
+				// or maybe pausing on interim transcript is enough
+				if o.orchestrateOptions.onSpeakingStateChanged != nil {
+					o.orchestrateOptions.onSpeakingStateChanged(true)
+				}
+			}),
+			speechtotext.WithSpeechEndedCallback(func() {
+				if o.orchestrateOptions.onSpeakingStateChanged != nil {
+					o.orchestrateOptions.onSpeakingStateChanged(false)
+				}
+			}),
+			speechtotext.WithInterimTranscriptionCallback(func(transcript string) {
+				// TODO: Start generating interruption here already
+				// marking the ID will probably be required to keep track of it
+
+				if o.orchestrateOptions.onInterimTranscription != nil {
+					o.orchestrateOptions.onInterimTranscription(transcript)
+				}
+			}),
+			speechtotext.WithTranscriptionCallback(func(transcript string) {
+				if o.orchestrateOptions.onInterimTranscription != nil {
+					o.orchestrateOptions.onInterimTranscription("")
+				}
+
+				o.SendPrompt(transcript)
+			}),
+		}
+		if o.audioInput != nil {
+			sttOptions = append(sttOptions, speechtotext.WithEncodingInfo(o.audioInput.EncodingInfo()))
+		}
+
+		if err := o.speechToTextClient.Transcribe(context.TODO(), sttOptions...); err != nil {
+			log.Fatalf("Failed to start transcribing: %v", err)
+		}
+	}
+}
+
+func (o *Orchestrator) processUserTurn(prompt string) {
+	var interruptionID *int64
+	if o.turns.activeTurn() != nil {
+		interruptionID = utils.Ptr(time.Now().UnixNano())
+		interruption := &llms.InterruptionV0{
+			ID:     *interruptionID,
+			Source: prompt,
+		}
+		o.turns.addInterruption(*interruption)
+	}
+
+	passthrough := &prompt
+	if interruptionID != nil {
+		if o.interruptionHandlerV1 != nil {
+			if interruption, err := o.interruptionHandlerV1.HandleV1(*interruptionID, o, o.tools); err != nil {
+				log.Printf("Failed to handle interruption: %v", err)
+			} else {
+				o.turns.updateInterruption(*interruptionID, func(update *llms.InterruptionV0) {
+					update.Type = interruption.Type
+					update.Resolved = interruption.Resolved
+				})
+				return
+			}
+		} else if o.interruptionHandlerV0 != nil {
+			if err := o.interruptionHandlerV0.HandleV0(prompt, o.turns.turns, o.tools, o); err != nil {
+				log.Printf("Failed to handle interruption: %v", err)
+			} else {
+				o.turns.updateInterruption(*interruptionID, func(interruption *llms.InterruptionV0) {
+					interruption.Resolved = true
+				})
+				return
+			}
+		} else if o.interruptionClassifier != nil {
+			interruption, err := o.interruptionClassifier.Classify(prompt, llms.ToMessages(o.turns.turns), ClassifyWithTools(o.tools))
+			if err != nil {
+				// TODO: Retry?
+				log.Printf("Failed to classify interruption: %v", err)
+			} else {
+				o.turns.updateInterruption(*interruptionID, func(i *llms.InterruptionV0) { i.Type = string(interruption) })
+				passthrough, err = o.respondToInterruption(prompt, interruption)
+				if err != nil {
+					log.Printf("Failed to respond to interruption: %v", err)
+				}
+			}
+		}
+		o.turns.updateInterruption(*interruptionID, func(interruption *llms.InterruptionV0) {
+			interruption.Resolved = true
+		})
+	}
+	if passthrough != nil {
+		o.queuePrompt(*passthrough)
+	}
+}
+
+func (o *Orchestrator) queuePrompt(prompt string) {
+	if o.orchestrateOptions.onTranscription != nil {
+		o.orchestrateOptions.onTranscription(prompt)
+	}
+	o.transcripts <- prompt
+}
